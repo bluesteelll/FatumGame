2c24b1486a8053079649aff372d441898f5d3837
Celtokisa <bluesteelll@hotmail.com>
2026-01-27 20:38:12 +0300

    Enace item impl

---
+++ Plugins/Enace/Source/Enace/Public/Enace.h
@@ -0,0 +1,19 @@
+// Copyright 2025 Oversized Sun Inc. All Rights Reserved.
+// Enace - Gameplay Data Storage System
+// Convenience header - includes all public Enace types
+
+#pragma once
+
+// Core
+#include "EnaceModule.h"
+#include "EnaceDispatch.h"
+
+// Items
+#include "Items/EnaceItemTypes.h"
+#include "Items/EnaceItemDefinition.h"
+
+// Spawners
+#include "Spawners/EnaceItemSpawner.h"
+
+// Tags
+#include "Tags/EnaceTags.h"
